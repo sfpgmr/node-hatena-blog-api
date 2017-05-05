ab8a0dc441afa1ebeb43e3bb6d5e3a0a5bcddd91
sfpgmr <sfpgmr@example.com>
2017-05-06 05:57:30 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const rollup = require('rollup').rollup;
 const gulpRollup = require('gulp-rollup');
 const nodeResolve = require('rollup-plugin-node-resolve');
@@ -7,15 +6,6 @@ const commonjs = require('rollup-plugin-commonjs');
 
 const paths = {
   src: './src/**/*.js',
-  test: './test/**/*.js',
-  testFixtures: './test/*.png',
-  coverage: './coverage/**/lcov.info',
-  coverageDir: './coverage/',
-  compiledDir: './.tmp/',
-  compiledSrc: './.tmp/src/**/*.js',
-  compiledSrcDir: './.tmp/src/',
-  compiledTest: './.tmp/test/**/*.js',
-  compiledTestDir: './.tmp/test/',
   buildDir: './lib/',
   exampleDir: './examples/node_modules/hatena-blog-api2/lib/'
 };
@@ -42,13 +32,6 @@ gulp.task('clean', function(done) {
   ], done);
 });
 
-gulp.task('coveralls', function() {
-  let coveralls = require('gulp-coveralls');
-  return gulp
-    .src(paths.coverage)
-    .pipe(coveralls());
-});
-
 gulp.task('build', function() {
   rollup(rollupParams).then(bundle=>{
     bundle.write({
@@ -65,44 +48,6 @@ gulp.task('example',()=>{
       dest: `${paths.exampleDir}index.js`
     });
   });
-});
-
-gulp.task('compile-src', function() {
-  rollup(rollupParams).then(bundle=>{
-    bundle.write({
-      format: 'cjs',
-      dest: `${paths.compiledSrcDir}index.js`
-    });
-  });
-});
-
-gulp.task('copy-test-fixtures', () =>
-  gulp
-    .src(paths.testFixtures)
-    .pipe(gulp.dest(paths.compiledTestDir))
-);
-
-gulp.task('compile-test', ['copy-test-fixtures'], function() {
-//  let espower = require('gulp-espower');
-  return 
-    gulpRollup(rollupParams)
-    .pipe('index.js')
-    .pipe(espower())
-    .pipe(gulp.dest(paths.compiledTestDir));
-});
-
-gulp.task('test', ['compile-src', 'compile-test'], function() {
-  let istanbul = require('gulp-istanbul');
-  let mocha = require('gulp-mocha');
-  return gulp
-    .src(paths.compiledSrc)
-    .pipe(istanbul())
-    .on('finish', () =>
-      gulp
-        .src(paths.compiledTest)
-        .pipe(mocha().on('error', gutil.log))
-        .pipe(istanbul.writeReports(paths.coverageDir))
-  );
 });
 
 gulp.task('watch', gulp.watch.bind(gulp,[paths.src], ['build','example']));
